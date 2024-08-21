574daca29f45bdb15b4a91b42a2cb4411d419539
39sanskar <mishrasanskar496@gmail.com>
2024-08-22 03:30:07 +0530

    solution

--- assignments-master/01-js/easy/anagram.js
+++ assignments-master/01-js/easy/anagram.js
@@ -5,6 +5,9 @@
 */
 
 function isAnagram(str1, str2) {
+  const cleanStr = (str) => str.replace(/\s/g,'').toLowerCase();
+  const sortedStr = (str) => cleanStr(str).split('').sort().join('');
+  return sortedStr(str1) === sortedStr(str2);
 
 }
 
